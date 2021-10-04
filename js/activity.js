2a7a4bdb1dbe14abf2b593174e76d46d79ba9b58
Jimmy Tang <superjimmyman@jimmys-mbp.lan>
2021-10-04 00:50:18 -0700

    tablegrid1

--- js/activity.js
+++ js/activity.js
@@ -4,4 +4,33 @@ $(document).ready(function(){
     $("tr td.clickit").click(function(){
       $(this).toggleClass("afterclick");
     }) 
+
+
+
+
+    //User interaction with table cells
+
+    $("td").click(function () {
+        var content = $(this).text();
+
+        if (content != "Not Available") {
+            $("tr.clickit").toggleClass("afterclick");
+            
+            if ($(this).hasClass("afterclick")) {
+                $('#displaySelected').css("visibility" ,"visible");
+                $('#displaySelected').css("margin-top", "2em");
+                $('#result').append("<p>" + content + "</p>");
+            } else {
+               $('#result p:contains('+content+')').remove();
+
+               if ($('#result').has('p').length == false) {
+                   $('#displaySelected').css("visibility", "hidden");
+                   $('#displaySelected').css("margin-top", "0");
+               }
+            }
+        }
+    });
+    
   });
+
+  
